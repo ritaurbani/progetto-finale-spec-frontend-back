33f0d16371ffb0acd603ecc16e55df315d786db1
ritaurbani <ritaurbani@gmail.com>
2025-06-13 22:37:50 +0200

    type

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3002;
+const PORT = 3001;
 
 // Middleware
 app.use(
